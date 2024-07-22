6f22a2e5c29deb4aafb7ebc169db1efbad282694
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2024-07-22 14:31:35 +0200

    finished power calculations - to do: write-up

--- PAP/code/S2P_power_sims.R
+++ PAP/code/S2P_power_sims.R
@@ -12,7 +12,7 @@ dta$maize_acre[dta$maize_acre > 10] <- NA
 dta$yield <- dta$maize_hrv*60/dta$maize_acre*2.5
 
 dta$yield[dta$yield >8500] <- NA
-dta$yield[dta$yield < 500] <- NA
+dta$yield[dta$yield < 1500] <- NA
 dta <- subset(dta,!is.na(dta$yield))
 mean(dta$yield, na.rm=T)
 sd(dta$yield, na.rm=T)
@@ -21,16 +21,16 @@ sum(!is.na(dta$yield))
 
 library(randomizr)    # randomizr package for complete random assignment
 
-possible.ns <- seq(from=.2, to=.8, by=0.1)
-possible.ns2 <- seq(from=.2, to=.8, by=0.1)
+possible.ns <- seq(from=.2, to=.8, by=0.05)
+possible.ns2 <- seq(from=.2, to=.8, by=0.05)
 
 
 power.atleastone <-  matrix(NA, length(possible.ns),length(possible.ns2))  
 power.alltreatments <-  matrix(NA, length(possible.ns),length(possible.ns2))  
 
-alpha <- 0.5
+alpha <- 0.05
 sims <- 1000
-N <- 1000
+N <- 2000
 
 #### Outer loop to vary the number of subjects ####
 for (j in 1:length(possible.ns)){
@@ -45,77 +45,132 @@ for (j in 1:length(possible.ns)){
 
   #### Inner loop to conduct experiments "sims" times over for each N ####
   for (i in 1:sims){
-    Y0 <-  sample(dta$yield, size=N)
+    Y0 <-  sample(dta$yield, size=N, replace = TRUE)
     Y0[Y0<0] <- 0
-    tau_1 <-  mean(Y0, na.rm=T)*.06
-    tau_2 <-  mean(Y0, na.rm=T)*.06
-    tau_3 <-  mean(Y0, na.rm=T)*.10
+    tau_1 <-  mean(Y0, na.rm=T)*.1
+    tau_2 <-  mean(Y0, na.rm=T)*.1
+    tau_3 <-  mean(Y0, na.rm=T)*.25
+    tau_4 <-  mean(Y0, na.rm=T)*.40
     
     Y1 <- Y0 + tau_1
     Y2 <- Y0 + tau_2
     Y3 <- Y0 + tau_3
+    Y4 <- Y0 + tau_4
     
-    Z.sim <- complete_ra(N=1000,prob_each = c((1-shares_C)*shares_T1/2, (1-shares_C)*shares_T1/2,(1-shares_C)*(1-shares_T1), shares_C) )
-    Y.sim <- Y0*(Z.sim=="T4")  + Y1*(Z.sim=="T1")  + Y2*(Z.sim=="T2") + Y3*(Z.sim=="T3")
+    Z.sim <- complete_ra(N=N,prob_each = c((1-shares_C)*shares_T1/2, (1-shares_C)*shares_T1/2,(1-shares_C)*(1-shares_T1)/2,(1-shares_C)*(1-shares_T1)/2, shares_C) )
+    Y.sim <- Y0*(Z.sim=="T5")  + Y1*(Z.sim=="T1")  + Y2*(Z.sim=="T2") + Y3*(Z.sim=="T3") +  Y4*(Z.sim=="T4")
+    sim.dta <- data.frame(Y.sim,Z.sim)
+    fit.sim1 <- lm(Y.sim ~(Z.sim=="T1") + (Z.sim=="T2")+ (Z.sim=="T3" | Z.sim=="T4" ) +( Z.sim=="T4" ), data=sim.dta)
 
-    fit.sim <- lm(Y.sim ~(Z.sim=="T1") + (Z.sim=="T2")+ (Z.sim=="T3"))
-
     
-    p.T1vsC <- summary(fit.sim)$coefficients[2,4]
-    p.T2vsC <- summary(fit.sim)$coefficients[3,4]
-    p.T3vsC <- summary(fit.sim)$coefficients[4,4]
     
-    significant.experiments[i] <- (p.T1vsC < alpha) & (p.T2vsC < alpha) & (p.T3vsC < alpha)
-    one.significant.experiment[i] <- (p.T1vsC < alpha) | (p.T2vsC < alpha) | (p.T3vsC < alpha)
+    p.T1vsC <- summary(fit.sim1)$coefficients[2,4]
+    p.T2vsC <- summary(fit.sim1)$coefficients[3,4]
+    p.T3vsC <- summary(fit.sim1)$coefficients[4,4]
+    p.T3vsP4 <- summary(fit.sim1)$coefficients[5,4]
+    
+    
+    
+    significant.experiments[i] <- (p.T1vsC < alpha) & (p.T2vsC < alpha) & (p.T3vsC < alpha) & (p.T3vsP4 < alpha)
+    one.significant.experiment[i] <- (p.T1vsC < alpha) | (p.T2vsC < alpha) | (p.T3vsC < alpha) | (p.T3vsP4 < alpha)
     ###now add and extra group for 
   }
   power.alltreatments[j,k] <- mean(significant.experiments)
-  power.atleastone[j,k] <- mean(significant.experiments)
+  power.atleastone[j,k] <- mean(one.significant.experiment)
   print(k)
   }
 print(j)  
 }
 
-png(file="power_plot1.png",
-    width=600, height=350)
+png(file="contour_plot.png",   width=600, height=350)
 
-plot(type = "l",possible.ns,  power.T1vsC, ylim=c(0,1))
-lines(possible.ns,  power.T2vsC, col="red")
-lines(possible.ns, power.interact, col="blue")
-lines(possible.ns, power.S2P, col="orange")
-abline(h=0.8)
+contour(possible.ns2,possible.ns,t(power.alltreatments))
+contour(possible.ns2,possible.ns,t(power.alltreatments), levels = c(0.8), add = TRUE, col = "red", lwd = 2)
 
 dev.off()
 
-possible.ns <- seq(from=100, to=1500, by=10)     # The sample sizes we'll be considering
-powers <- rep(NA, length(possible.ns))           # Empty object to collect simulation estimates
+possible.ss <- seq(from=200, to=1900, by=10)     # The sample sizes we'll be considering
+power.all <- rep(NA, length(possible.ss))  
+power.one <- rep(NA, length(possible.ss))  
+power.T1 <- rep(NA, length(possible.ss))  
+power.T2 <- rep(NA, length(possible.ss))  
+power.T3 <- rep(NA, length(possible.ss))  
+power.T4 <- rep(NA, length(possible.ss))  
 alpha <- 0.05                                    # Standard significance level
 sims <- 1000                                      # Number of simulations to conduct for each N
 
+### fill in optimized shares from previous 
+j <- 4
+k <- 9
+
+shares_C <- possible.ns[j]
+shares_T1 <- possible.ns[k]
+
+
 #### Outer loop to vary the number of subjects ####
-for (j in 1:length(possible.ns)){
-  N <- possible.ns[j]                              # Pick the jth value for N
+for (j in 1:length(possible.ss)){
+  N <- possible.ss[j]                              # Pick the jth value for N
   
   significant.experiments <- rep(NA, sims)         # Empty object to count significant experiments
+  one.significant.experiment <- rep(NA, sims)         # Empty object to count significant experiments
+  T1.sig <- rep(NA, sims)         # Empty object to count significant experiments
+  T2.sig <- rep(NA, sims)         # Empty object to count significant experiments
+  T3.sig <- rep(NA, sims)         # Empty object to count significant experiments
+  T4.sig <- rep(NA, sims)         # Empty object to count significant experiments
   
   #### Inner loop to conduct experiments "sims" times over for each N ####
   for (i in 1:sims){
-    Y0 <-  sample(size = N, dta$yield)+tau_1+tau_2+tau_3              # control potential outcome
-    tau <- (mean_base_outcome+tau_1+tau_2+tau_3)*.1                                       # Hypothesize treatment effect
-    Y1 <- Y0 + tau                                 # treatment potential outcome
-    Z.sim <- rbinom(n=N, size=1, prob=.5)          # Do a random assignment
-    Y.sim <- Y1*Z.sim + Y0*(1-Z.sim)               # Reveal outcomes according to assignment
-    fit.sim <- lm(Y.sim ~ Z.sim)                   # Do analysis (Simple regression)
-    p.value <- summary(fit.sim)$coefficients[2,4]  # Extract p-values
-    significant.experiments[i] <- (p.value <= alpha) # Determine significance according to p <= 0.05
+    Y0 <-  sample(dta$yield, size=N, replace = TRUE)
+    Y0[Y0<0] <- 0
+    tau_1 <-  mean(Y0, na.rm=T)*.1
+    tau_2 <-  mean(Y0, na.rm=T)*.1
+    tau_3 <-  mean(Y0, na.rm=T)*.25
+    tau_4 <-  mean(Y0, na.rm=T)*.40
+    
+    
+    Y1 <- Y0 + tau_1
+    Y2 <- Y0 + tau_2
+    Y3 <- Y0 + tau_3
+    Y4 <- Y0 + tau_4
+    
+    Z.sim <- complete_ra(N=N,prob_each = c((1-shares_C)*shares_T1/2, (1-shares_C)*shares_T1/2,(1-shares_C)*(1-shares_T1)/2,(1-shares_C)*(1-shares_T1)/2, shares_C) )
+    Y.sim <- Y0*(Z.sim=="T5")  + Y1*(Z.sim=="T1")  + Y2*(Z.sim=="T2") + Y3*(Z.sim=="T3") +  Y4*(Z.sim=="T4")
+    sim.dta <- data.frame(Y.sim,Z.sim)
+    fit.sim1 <- lm(Y.sim ~(Z.sim=="T1") + (Z.sim=="T2")+ (Z.sim=="T3" | Z.sim=="T4" ) +( Z.sim=="T4" ), data=sim.dta)
+    
+    
+    p.T1vsC <- summary(fit.sim1)$coefficients[2,4]
+    p.T2vsC <- summary(fit.sim1)$coefficients[3,4]
+    p.T3vsC <- summary(fit.sim1)$coefficients[4,4]
+    p.T3vsP4 <- summary(fit.sim1)$coefficients[5,4]
+    
+    
+    
+    significant.experiments[i] <- (p.T1vsC < alpha) & (p.T2vsC < alpha) & (p.T3vsC < alpha) & (p.T3vsP4 < alpha)
+    one.significant.experiment[i] <- (p.T1vsC < alpha) | (p.T2vsC < alpha) | (p.T3vsC < alpha) | (p.T3vsP4 < alpha)
+    T1.sig[i] <- (p.T1vsC < alpha)
+    T2.sig[i] <- (p.T2vsC < alpha)
+    T3.sig[i] <- (p.T3vsC < alpha)
+    T4.sig[i] <- (p.T3vsP4 < alpha)
   }
   
-
+  power.all[j] <- mean(significant.experiments)
+  power.one[j] <- mean(one.significant.experiment)    
+  power.T1[j] <- mean(T1.sig)    
+  power.T2[j] <- mean(T2.sig)   
+  power.T3[j] <- mean(T3.sig)  
+  power.T4[j] <- mean(T4.sig)  
 }
 
-png(file="power_plot2.png",
-    width=600, height=350)
+png(file="power_curves.png",   width=600, height=350)
 
-plot(type = "l",possible.ns,  powers, ylim=c(0,1), col="green")
+plot(type = "l",possible.ss,  power.all, ylim=c(0,1), col="black")
+lines(possible.ss,power.T1, col="green")
+lines(possible.ss,power.T2, col="red")
+lines(possible.ss,power.T3, col="blue")
+lines(possible.ss,power.T4, col="orange")
 abline(h=0.8)
 dev.off()
+
+### finally fill in sample size to get the groups:
+table(complete_ra(N=1800,prob_each = c((1-shares_C)*shares_T1/2, (1-shares_C)*shares_T1/2,(1-shares_C)*(1-shares_T1)/2,(1-shares_C)*(1-shares_T1)/2, shares_C) ))
