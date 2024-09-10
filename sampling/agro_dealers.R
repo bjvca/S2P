7f5be8668f8171fe4ed94d36b4f895975eb38877
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2024-09-10 17:55:37 +0200

    started matching villages to agro-shops using openrouteservices API

---
+++ sampling/agro_dealers.R
@@ -0,0 +1,18 @@
+library(sf)
+library(mapview)
+#library(geojsonio)
+library(sp)
+library(sf)
+
+agro_shops <- st_layers("FWL and AGORA Map.kml")
+
+agro_shops <- st_read("FWL and AGORA Map.kml", layer = "FWL Coordinates-Central Region")
+
+mapviewOptions(fgb = FALSE)
+agro_shops <- data.frame(agro_shops)
+sel_shops <- agro_shops[c("Address","Latitude",  "Longitude") ]
+
+###issue with naming (weird character)
+sel_shops[56,1] <- "Suza (Kasungu)"
+
+write.csv(sel_shops, file = "shops_gps.csv", row.names = FALSE)
