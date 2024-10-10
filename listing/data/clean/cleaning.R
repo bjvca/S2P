8cfe0a0a8339bc0a0a680461a698ebda24898ac7
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2024-10-10 08:57:35 -0400

    fixinging some duplicate soil sample IDs

--- listing/data/clean/cleaning.R
+++ listing/data/clean/cleaning.R
@@ -52,4 +52,71 @@ dta$farmer_name.y <- NULL
 dta$treat <- NULL
 dta <- merge(dta, dta_list[c("farmer_ID","treat")], by.x = "Farmer_ID", by.y="farmer_ID")
 
+## correcting some duplicates
+dta$Farmer_ID[dta$X_uuid  == "faf01a89-e09f-4f8b-af2b-c8a3b42b4e0f"] <- "F_998"
+dta$Farmer_ID[dta$X_uuid  == "7fac07b6-a9ae-4b89-b3af-ef70727adfd0"] <- "F_3537"
+
+dta$Farmer_ID[dta$X_uuid  == "a70fc0a5-83ea-4373-afc6-b80646d5a7e1"] <- "F_2702"
+dta$farmer_name[dta$X_uuid  == "a70fc0a5-83ea-4373-afc6-b80646d5a7e1"] <- "Florence Banda (F_2702)"
+
+dta$Farmer_ID[dta$X_uuid  == "5088423b-43a2-4cac-91a6-f80e745b7610"] <- "F_3414"
+dta$farmer_name[dta$X_uuid  == "5088423b-43a2-4cac-91a6-f80e745b7610"] <- "WATSON KASANTHI (F_3414)"
+dta$Farmer_ID[dta$X_uuid  == "9fbe6647-19b3-4db9-987f-6b3f6f6ab0ad"] <- "F_374"
+
+dta <- subset(dta, !(dta$phone_1  == "985769405" & dta$Farmer_ID ==  "F_455"))
+
+dta$Farmer_ID[dta$X_uuid  == "df68dad5-ddf7-4b79-baef-ae6c332c8f09"] <- "F_11"
+dta$farmer_name[dta$X_uuid  == "df68dad5-ddf7-4b79-baef-ae6c332c8f09"] <-  "Aubrey Ackison Kuleza"
+
+dta$Farmer_ID[dta$X_uuid  == "97f716a6-150d-4f35-87dc-9de3a5e4fa1c"] <-  "F_686"
+dta$Farmer_ID[dta$X_uuid  == "952a21f0-ae17-43a0-ac5a-5b1b7d197f9e"] <-  "F_636"
+
+
+sum(duplicated(dta$Farmer_ID))  #0 duplicates
+
+## get latest names if farmers were replaced
+##if q1 = no
+dta$farmer_name[dta$q1=="No"] <- dta$New_farmer_name[dta$q1=="No"] 
+dta$New_farmer_name <- NULL
+
+dta$farmer_name[dta$check.dec_maker=="No"] <- dta$check.name_dec_maker[dta$check.dec_maker=="No"] 
+column_names <- c(
+  "Farmer_ID",
+  "farmer_name", "enumerator", "district", "shop_name", 
+  "farmer_dist", "TA", "village", "check.maize.mer_farm_ID", 
+  "check.maize.phone_1", "check.maize.phone_2", "check.maize.test_date", 
+  "check.maize._location_latitude", "check.maize._location_longitude", "treat","today"
+)
+
+
+###select variables we want to keep
+dta <- dta[column_names]
+
+#and give them sensible names
+names(dta) <-  c(  "Farmer_ID",  "farmer_name", "enumerator", "district", "shop_name", 
+  "farmer_dist", "TA", "village", "soil_sample_ID", 
+  "phone_1", "phone_2", "test_date", 
+  "hh_location_latitude", "hh_location_longitude", "treat","today"
+)
+
+##correct some duplicates and problems with soil_sample_IDs
+
+dta$soil_sample_ID[dta$soil_sample_ID == "15066759"] <-  "FWM-ML-52045"
+dta$soil_sample_ID[dta$soil_sample_ID == "1672299"] <- "FWM-ML-45381"
+dta$soil_sample_ID[dta$soil_sample_ID == "1754087"] <-  "FWM-ML-51189"
+dta$soil_sample_ID[dta$soil_sample_ID == "2211619"] <-  "FWM-ML-51859"
+dta$soil_sample_ID[dta$soil_sample_ID == "57242686"] <-  "FWM-ML-51329"
+dta$soil_sample_ID[dta$soil_sample_ID == "7232617"] <-  "FWM-ML-50947"
+dta$soil_sample_ID[dta$soil_sample_ID == "FW&-ML-&0339"] <-  "FWM-ML-51801"
+
+dta$soil_sample_ID[dta$Farmer_ID == "F_692"] <- "FWM-ML-51939"
+dta$soil_sample_ID[dta$Farmer_ID == "F_793"] <- "FWM-ML-45560"
+
+### still 2 duplicates
+sum(duplicated(dta$soil_sample_ID))
+### show duplicates
+write.csv(dta[dta$soil_sample_ID %in% dta$soil_sample_ID[duplicated(dta$soil_sample_ID)],],file="dupl_barcodes.csv")
+
 write.csv(dta,paste(path,"", sep="/data/clean/linkfile.csv"), row.names = FALSE)
+
+
